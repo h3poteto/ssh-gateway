648a3f41403681844dea2a60ec6fac9ddf2488e3
AkiraFukushima <h3.poteto@gmail.com>
2019-06-07 22:23:34 +0900

    Initial commit

---
+++ puppet/ssh.pp
@@ -0,0 +1,9 @@
+exec { "apt-get update":
+  command => "/usr/bin/apt-get update",
+  onlyif  => "/bin/sh -c '[ ! -f /var/cache/apt/pkgcache.bin ] || /usr/bin/find /etc/apt/* -cnewer /var/cache/apt/pkgcache.bin | /bin/grep . > /dev/null'",
+}
+
+package { 'ssh':
+  ensure => installed,
+  require => Exec['apt-get update'],
+}
